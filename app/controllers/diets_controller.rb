1b4a4ac02c3283fd12504a6cf1142be13513cdae
abdiasviana <abdviana@gmail.com>
2020-06-16 22:50:48 -0300

    Adicionado Bootstrap para estilizar as views, criado a view de cadastro de novas dietas, ainda simples mas será modelada para ficar como o mockup

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -1,6 +1,13 @@
 class DietsController < ApplicationController
+
     def index
         @diets = Diet.order(:refeicao)
         @infos = Info.order(:data_inicio)
+    end
+
+    def create
+        diet = params.require(:diet).permit(:horario, :refeicao, :descricao)
+        Diet.create diet
+        redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   root to: "diets#index"
+
+  post "diets", to: "diets#create"
+  get "diets/new", to: "diets#new"
+
 end
